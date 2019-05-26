992b3928f00b24b535feedc8d72bf1ef9458e5f5
amirhosseinrohafza55 <46925170+amirhosseinrohafza55@users.noreply.github.com>
2019-05-26 20:36:27 +0430

    Add files via upload

---
+++ sliders-master/sliders/js/index.js
@@ -0,0 +1,66 @@
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+   
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
+// contoroll
+var slideIndex = 1;
+controlSlides(slideIndex);
+
+function plusSlides(n) {
+    controlSlides(slideIndex += n);
+}
+function currentSlide(n) {
+    controlSlides(slideIndex = n);
+}
+function controlSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+  }
+
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+  }
+  var modal=document.getElementById("id01");
+  function login(){
+    
+   modal.style.display="block";
+  }
+  function close_loginpage(){
+ 
+modal.style.display="none";
+  }
+  window.onclick=function(event){
+    if(event.target==modal){
+      modal.style.display="none";
+    }
+  }
